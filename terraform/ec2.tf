81e0b12b9f7e703e8b8f2d2f84ed0d30aa6834f8
Anji399 <mvpareddy3493@gmail.com>
2023-06-28 15:31:00 +0530

    sec

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,7 +2,7 @@
      ami = var.imagename
      availability_zone = "us-east-1a"
      instance_type = "t2.micro"
-     key_name = "feb"
+     key_name = "new"
      subnet_id = "${aws_subnet.my-pub-1.id}"
      vpc_security_group_ids = ["${aws_security_group.my-sg.id}"]
      associate_public_ip_address = true	
